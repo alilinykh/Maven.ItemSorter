a73b895e7e814f5c61eb051652171a5086107de3
alilinykh <aililnykh@dtcc.edu>
2019-06-06 15:09:49 -0400

    lab done

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,23 @@
 package rocks.zipcode.io;
 
-import java.util.Comparator;
+import com.sun.tools.javac.jvm.Items;
+
+import java.lang.reflect.Array;
+import java.util.*;
 
 /**
  * @author leon on 30/01/2019.
  */
-public class ItemSorter {
+public class ItemSorter{
+    Item[] items;
+
     public ItemSorter(Item[] items) {
+        this.items = items;
+
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Arrays.sort(items,comparator);
+        return items;
     }
 }
